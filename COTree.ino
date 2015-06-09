3a5fffa7845ddedc939c0440b546ad9a03c29759
Nikolaj Mikkelsen <nikolaj.c.mikkelsen@gmail.com>
2015-06-09 15:54:59 +0200

    Fixed timer and added a lot of stuff for testing

--- COTree.ino
+++ COTree.ino
@@ -10,10 +10,11 @@ int cVal = 0;
 //Threshold value
 int tVal = 0;
 //The value moving the threshold
-int mVal = 0;
+//Lower this to increase the time between wither and stretch cycles. Increase it for the opposite effect.
+int mVal = 1;
 
 //Average value
-int aVal = 0;
+float aVal = 0;
 //List of values
 int values[5];
 //Pointing at the next value to be updated
@@ -21,9 +22,9 @@ int valuesPointer = 0;
 
 
 //Last Time
-int lTime = 0;
+unsigned long lTime = 0;
 //Delay value in seconds
-int dTime = 30;
+long dTime = 5;
 
 void setup() {
 	//Setting pinmode on the wither and stretch pins
@@ -36,22 +37,32 @@ void setup() {
 	}
 
 	//Adjusting the last time variable so the loop will run at start of code instead of waiting 'dTime' seconds
-	lTime = millis() - (dTime * 1000);
+	//lTime = millis() - (dTime * 1000);
+
+	//For testing purposes
+	Serial.begin(9600);
 }
 
 void loop() {
 	//Checking if enough time has passed
 	if(millis() - lTime > (dTime * 1000)){
 		updateVals();
-
+		printVals();
 		//If the current value is equal to or larger than the threshold value the wither/stretch sequence will be triggered.
 		//Else the threshold value will be lowered according to mVal to increase the likelyhood of a reaction from COTree
+		
 		if(cVal >= tVal){
-			wither();
-			tVal = cVal;
+			simpleCycle(2, 0.5, 2, 0.5);
+			//tVal = cVal;
+                        
+                        tVal = cVal + ((60/dTime)*mVal);
 		} else{
 			tVal -= mVal;
 		}
+		
+
+
+		checkSerial();
 
 	//Updating the timer variable
   	lTime = millis();
@@ -65,8 +76,15 @@ void loop() {
 //Makes the leaves wither for wT seconds, then after a delay of dT seconds it will stretch the leaves for sT seconds. 
 //After that it will cool of for cT seconds before returning to normal behaviour. 
 void simpleCycle(int wT, int dT, int sT, int cT){
+        Serial.println("***** WITHER *****");
+        Serial.println();
 	wither(wT);
+        Serial.println("***** DELAY *****");
+        Serial.println();
 	delay(dT * 1000);
+
+        Serial.println("***** STRETCH THEN DELAY *****");
+        Serial.println();
 	stretch(sT, cT);
 }
 
@@ -114,19 +132,74 @@ void stretchPulse(){
 // ----- Pulse movement methods ends here ----- //
 
 
-
+//Updates all the values
 void updateVals(){
 	//Updating the current value
 	cVal = analogRead(cPin);
 
 	//Inserting the current value into the array of values
 	values[valuesPointer] = cVal;
-	values = (values + 1) % 5;
+        valuesPointer++;
+	valuesPointer %= 5;
 
 	//Calculating and updating the average value
 	aVal = 0;
 	for(int i=0; i<5; i++){
-	    aVal = values[i];
+	    aVal += values[i];
 	}
 	aVal /= 5;
+}
+
+//Prints all the value. Use for testing/debugging
+void printVals(){
+	Serial.println("----------");
+	Serial.println("Printing all values:");
+	Serial.println();
+
+	Serial.print("Current value:");
+	Serial.println(cVal);
+	Serial.println();
+
+	Serial.print("Threshold value:");
+	Serial.println(tVal);
+	Serial.print("Offset value:");
+	Serial.println(mVal);
+	Serial.println();
+	
+	Serial.print("Last 5 values:");
+        for(int i = 0; i<5; i++){
+          Serial.print(values[i]);
+          Serial.print("     ");
+        }
+        Serial.println();
+	Serial.print("The average value:");
+	Serial.println(aVal);
+
+	Serial.println();
+	Serial.println("Done printing values");
+	Serial.println("----------");
+	Serial.println();
+}
+
+//For testing purposes
+void checkSerial(){
+	char c;
+	int wT = 2;
+	int dT = 2;
+	int sT = 2;
+	int cT = 2;
+
+	if(Serial.available()){
+		c = Serial.read();
+		if(c == 'w'){
+			wither(wT);
+		} else if(c == 's'){
+			stretch(sT, cT);
+		} else if(c == 'c'){
+			simpleCycle(wT, dT, sT, cT);
+		}
+		while(Serial.available()){
+			Serial.read();
+		}
+	}
 }
